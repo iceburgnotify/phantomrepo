357ba83c2056c9db157c9be5781c90a4a8d39ec4
admin <root@localhost>
2018-09-25 07:10:33 +0000

    Saved: modified playbook

--- GitTest.py
+++ GitTest.py
@@ -89,8 +89,8 @@ def add_1(action=None, success=None, container=None, results=None, handle=None, 
 
 def call_api_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
     phantom.debug('call_api_1() called')
-    domain = phantom.collect2(container=container,datapath=['action_result.parameter.second_numb'],action_results=results)
-    phantom.debug(domain)
+    # domain = phantom.collect2(container=container,datapath=['action_result.parameter.second_numb'],action_results=results)
+    phantom.debug(results)
     return
     return
 
