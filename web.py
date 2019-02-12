2b7c8f6bd4a9ffd78f13315bed04a5d0b58f8500
admin <root@localhost>
2019-02-12 15:35:01 +0000

    modified playbook

---
+++ web.py
@@ -0,0 +1,45 @@
+"""
+"""
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+##############################
+# Start - Global Code Block
+
+import requests, bs4
+
+# End - Global Code block
+##############################
+
+def on_start(container):
+    phantom.debug('on_start() called')
+    
+    # call 'webscrapping' block
+    webscrapping(container=container)
+
+    return
+
+def webscrapping(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('webscrapping() called')
+    
+    response = requests.get("https://en.wikipedia.org/wiki/List_of_Olympic_medalists_in_judo")	
+    htmlResponse = bs4.BeautifulSoup(response.text)
+    phantom.debug(htmlResponse)
+    return
+
+def on_finish(container, summary):
+    phantom.debug('on_finish() called')
+    # This function is called after all actions are completed.
+    # summary of all the action and/or all detals of actions 
+    # can be collected here.
+
+    # summary_json = phantom.get_summary()
+    # if 'result' in summary_json:
+        # for action_result in summary_json['result']:
+            # if 'action_run_id' in action_result:
+                # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
+                # phantom.debug(action_results)
+
+    return
