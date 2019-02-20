688fceae5d7b763b391c0816422425f395f06676
admin <root@localhost>
2019-02-20 06:42:23 +0000

    Saved: modified playbook

--- Phishing Triage.py
+++ Phishing Triage.py
@@ -17,43 +17,38 @@ domain ='paypal.account.myorder-manage.com/signin/'
 def on_start(container):
     phantom.debug('on_start() called')
     
-    # call 'whois_domain_1' block
-    whois_domain_1(container=container)
-
-    return
-
-def vt_url_reputation(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('vt_url_reputation() called')
-
-    # collect data for 'vt_url_reputation' call
-
-    parameters = []
-    
-    # build parameters list for 'vt_url_reputation' call
-    parameters.append({
-        'url': "",
-    })
-
-    phantom.act("url reputation", parameters=parameters, assets=['virustotal'], callback=pt_url_reputation, name="vt_url_reputation")
+    # call 'vt_url_reputation' block
+    vt_url_reputation(container=container)
 
     return
 
 def pt_url_reputation(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
     phantom.debug('pt_url_reputation() called')
-    
+    if not success:
+        phantom.debug('Error in url reputation of VirusTotal')
+        return
     #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    phantom.debug(results)
+    # collect data for 'pt_url_reputation' call
 
     parameters = []
+    
+    # build parameters list for 'pt_url_reputation' call
+    parameters.append({
+        'url': url,
+    })
 
-    phantom.act("url reputation", parameters=parameters, app={ "name": 'PhishTank' }, name="pt_url_reputation", parent_action=action)
+    phantom.act("url reputation", parameters=parameters, assets=['phishtank'], callback=geolocate_ip, name="pt_url_reputation", parent_action=action)
 
     return
 
 def whois_domain_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
     phantom.debug('whois_domain_1() called')
-
+    if not success:
+        phantom.debug('Error in geolocate ip of MaxMind')
+        return
     # collect data for 'whois_domain_1' call
-
+    phantom.debug(results)
     parameters = []
     
     # build parameters list for 'whois_domain_1' call
@@ -61,38 +56,72 @@ def whois_domain_1(action=None, success=None, container=None, results=None, hand
         'domain': domain,
     })
 
-    phantom.act("whois domain", parameters=parameters, assets=['whois'], callback=call_api_1, name="whois_domain_1", parent_action=action)
+    phantom.act("whois domain", parameters=parameters, assets=['whois'], callback=get_screenshot_1, name="whois_domain_1", parent_action=action)
 
     return
 
-def call_api_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('call_api_1() called')
+def geolocate_ip(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('geolocate_ip() called')
+    if not success:
+        phantom.debug('Error in url reputation of PhishTank')
+        return
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
     phantom.debug(results)
-    return
-
-def geolocate_ip_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('geolocate_ip_1() called')
-
-    parameters = []
-
-    phantom.act("geolocate ip", parameters=parameters, app={ "name": 'MaxMind' }, name="geolocate_ip_1", parent_action=action)
-
-    return
-
-def lookup_domain_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('lookup_domain_1() called')
-
-    # collect data for 'lookup_domain_1' call
+    # collect data for 'geolocate_ip' call
 
     parameters = []
     
-    # build parameters list for 'lookup_domain_1' call
+    # build parameters list for 'geolocate_ip' call
     parameters.append({
-        'domain': url,
-        'type': "mx",
+        'ip': "68.66.243.38",
     })
 
-    phantom.act("lookup domain", parameters=parameters, assets=['mxtoolbox'], callback=call_api_1, name="lookup_domain_1")
+    phantom.act("geolocate ip", parameters=parameters, assets=['maxmind'], callback=whois_domain_1, name="geolocate_ip", parent_action=action)
+
+    return
+
+def get_screenshot_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('get_screenshot_1() called')
+    if not success:
+        phantom.debug('Error in checking domain of whois')
+        return
+    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+    phantom.debug(results)
+    # collect data for 'get_screenshot_1' call
+
+    parameters = []
+    
+    # build parameters list for 'get_screenshot_1' call
+    parameters.append({
+        'url': "",
+        'size': "",
+    })
+
+    phantom.act("get screenshot", parameters=parameters, assets=['test ss'], callback=details, name="get_screenshot_1", parent_action=action)
+
+    return
+
+def details(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('details() called')
+    phantom.debug(results)
+    return
+
+def vt_url_reputation(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('vt_url_reputation() called')
+    if not success:
+        phantom.debug('Error in url reputation of VirusTotal')
+        return
+    phantom.debug(results)
+    # collect data for 'vt_url_reputation' call
+
+    parameters = []
+    
+    # build parameters list for 'vt_url_reputation' call
+    parameters.append({
+        'url': url
+    })
+
+    phantom.act("url reputation", parameters=parameters, assets=['virustest'], callback=pt_url_reputation, name="vt_url_reputation")
 
     return
 
