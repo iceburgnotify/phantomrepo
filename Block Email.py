3b676d55e93557a8f762b9a686d009d003c89158
admin <root@localhost>
2019-02-21 09:16:23 +0000

    Saved: modified playbook

--- Block Email.py
+++ Block Email.py
@@ -48,7 +48,7 @@ def block_sender_1(action=None, success=None, container=None, results=None, hand
     
     # build parameters list for 'block_sender_1' call
     parameters.append({
-        'id': "VI1P175MB0191ABD0857E66DFE334698CBA7E0@VI1P175MB0191.EURP175.PROD.OUTLOOK.COM",
+        'id': "VI1P175MB0191ABD0857E66DFE334698CBA7E0",
         'move_to_junk_folder': "",
         'email': "soartesting@outlook.com",
     })
